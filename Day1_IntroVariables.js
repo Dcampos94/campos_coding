2d1f7cf523d2bfdf18ea9d96eba0c1fdff728d50
Dcampos94 <dlcdcollector@gmail.com>
2014-01-10 16:53:58 -0500

    Day One Coding Work
    All of the first lines of code practiced and written in Web Fudamentals.

---
+++ Day1_IntroVariables.js
@@ -0,0 +1,57 @@
+/**
+ * Created by Daniel on 1/8/14.
+ */
+var numberOfTreesToCutDown=4;
+var x="Is a great letter!";
+var escapeCharacters="\"";
+var numberOfPies=99;
+var y=parseInt(prompt("Place a number here"));
+var z=6+8;
+var someCrasyNumber=parseInt(prompt("Place a number here"));
+var anotherCrazyNumber=parseInt(prompt("Place a number here"));
+var halfNumber=prompt("what is your favorite partial number?");
+var t=parseFloat(8);
+var u=6;
+var h=parseInt(t+u);
+var v="I added " +t+ " and " +u+ " and the answer was " +h+ ".";
+
+//console.log(x);
+//console.log(numberOfTreesToCutDown);
+//console.log(escapeCharacters);
+//console.log(3+y);
+//console.log(numberOfPies);
+//console.log(z);
+//console.log(numberOfTreesToCutDown+numberOfPies);
+//console.log(z/2);
+//console.log(z-6);
+//console.log(y%numberOfTreesToCutDown);
+//console.log(someCrasyNumber+anotherCrazyNumber);
+//console.log(235+"");
+//console.log(parseFloat(halfNumber) +10);
+//console.log(v);
+
+
+var shoppingList=["Eggs","Chicken","brownRice","Vegetables"];
+console.log(shoppingList);
+//console.log(shoppingList[0]);
+//console.log(shoppingList[1]);
+//console.log(shoppingList[2]);
+//console.log(shoppingList[3]);
+//console.log(shoppingList[4]);
+console.log(shoppingList.length);
+shoppingList[4]="Formula";
+console.log(shoppingList.length);
+shoppingList.push("Bacon");
+console.log(shoppingList.length);
+shoppingList.pop();
+
+console.log(shoppingList);
+//console.log(shoppingList[0]);
+//console.log(shoppingList[1]);
+//console.log(shoppingList[2]);
+//console.log(shoppingList[3]);
+//console.log(shoppingList[4]);
+//console.log(shoppingList[5]);
+
+
+
