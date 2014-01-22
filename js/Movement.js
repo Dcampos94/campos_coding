6ac43d246ae0c71df0379307c2ebbef9477a0b23
Dcampos94 <dlcdcollector@gmail.com>
2014-01-22 11:06:23 -0500

    Catchup work
    All work done so to catch back up after missing a day, using Justin's
    work as a guide.

--- js/Movement.js
+++ js/Movement.js
@@ -2,11 +2,13 @@
  * Created by Daniel on 1/15/14.
  */
 
-var xx=3;
-var yy=7;
+var yy=3;
+var xx=7;
 var whileCondition=true;
-
 var rowString="";
+var randomMonsterHealth=Math.floor((Math.random()*5)+5);
+var randomMonsterDamage=Math.floor((Math.random()*5)+5);
+var monsterMaker = makeMonster(world, monster, xx, yy);
 
 
 while(whileCondition==true){
@@ -24,6 +26,28 @@ while(whileCondition==true){
         console.clear();
         console.log(rowString);
 
+    if (monsterMaker!="No battles found.") {
+        var mobHealth="Health: "+randomMonsterHealth;
+        var mobDamage="Damage: "+randomMonsterDamage;
+    } else {
+        mobHealth="\t";
+        mobDamage="\t";
+    }
+    var statDisplay=
+
+        "Strength:      \t"+characterStats[0][0]+"\tExperience:\t"+characterStats[8][0]+"\t |"+
+            "\t"+ monsterMaker +"\n"+
+            "Dexterity:     \t"+characterStats[1][0]+"\tGold:\t\t"+characterStats[9][0]+"\t |"+
+            "\t"+mobHealth+"\n"+
+            "Intelligence:  \t"+characterStats[2][0]+"\t"+"\t"+"\t"+"\t"+" |"+
+            "\t"+mobDamage+"\n"+
+            "Stamina:       \t"+characterStats[3][0]+"\t"+"\t"+"\t"+"\t"+" |"+"\n"+"\t"+"\t"+"\t"+"\t"+"\t"+"\t |"+"\n"+
+
+            "Armor:         \t"+characterStats[6][0]+"\tHealth:\t\t"+characterStats[4][2]+"/"+characterStats[4][1]+"\t |"+"\n"+
+            "Damage:        \t"+characterStats[7][1]+"\tMana:\t\t"+characterStats[5][2]+"/"+characterStats[5][1]+"\t |";
+
+    console.log(statDisplay);
+
     var movement=prompt("Would you like to move Up, Down, Left, Right or Finish?").toLowerCase();
     if(movement=="up"){ xx--; }
     else if(movement=="down"){xx++;}
@@ -31,10 +55,9 @@ while(whileCondition==true){
     else if(movement=="right"){yy++;}
     else if(movement=="finish"){whileCondition=false}
     }
-//    console.log(world[xx][yy]);
 
-if(xx>=world.length){xx=0}
-else if(xx<0){xx=world.length-1}
-else if(yy>=world.length){yy=0}
+if(yy>=world.length){yy=0}
 else if(yy<0){yy=world.length-1}
+else if(xx>=world.length){xx=0}
+else if(xx<0){xx=world.length-1}
 
---
+++ js/RandomFunctions.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Daniel on 1/20/14.
+ */
+
+
+var str="I play League of Legends";
+var n=str.indexOf("o");
+console.log(n);
