e8fc45609fed0384fe6fea69a69c9f9f89a9c8f5
Dcampos94 <dlcdcollector@gmail.com>
2014-01-28 19:39:36 -0500

    Completion of Personal
    Personal conditional finished

--- Conditionals/Conditionals_Personal/js/script.js
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -6,3 +6,15 @@
 var hoursToSleep=parseInt(prompt("How many hours of sleep do you need?"));
 var hoursSlept=parseInt(prompt("How  many hours did you sleep today?"));
 var sleepEquation=hoursToSleep-hoursSlept;
+
+//Your hours slept should equal 0
+if (sleepEquation<0) {
+//console.log You didn't sleep enough
+    console.log("You haven't slept long enough.");
+} else {
+// if your hours slept is greater than or equal to 0
+    if (sleepEquation>=0) {
+//console log success
+        console.log("You slept a healthy amount, you should be ready for the day!");
+    }
+}
