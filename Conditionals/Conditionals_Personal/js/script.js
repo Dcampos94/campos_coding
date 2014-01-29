8e89e12dd8f08254a0d573438209a9bffeebe745
Dcampos94 <dlcdcollector@gmail.com>
2014-01-28 19:58:11 -0500

    Redo and completion
    Redo of the Personal Conditional after realizing that the original
    logic did not make sense.

--- Conditionals/Conditionals_Personal/js/script.js
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -3,18 +3,23 @@
  */
 
 //Setting up variables
-var hoursToSleep=parseInt(prompt("How many hours of sleep do you need?"));
-var hoursSlept=parseInt(prompt("How  many hours did you sleep today?"));
-var sleepEquation=hoursToSleep-hoursSlept;
-
-//Your hours slept should equal 0
-if (sleepEquation<0) {
+var hoursToSleep=parseInt(prompt("How many hours of sleep did you need this week?"));
+var hoursSleptMon=parseInt(prompt("How  many hours did you sleep on Monday?"));
+var hoursSleptTues=parseInt(prompt("How  many hours did you sleep on Tuesday?"));
+var hoursSleptWed=parseInt(prompt("How  many hours did you sleep on Wednesday?"));
+var hoursSleptThurs=parseInt(prompt("How  many hours did you sleep on Thursday?"));
+var hoursSleptFri=parseInt(prompt("How  many hours did you sleep on Friday?"));
+var hoursSleptSat=parseInt(prompt("How  many hours did you sleep on Saturday?"));
+var hoursSleptSun=parseInt(prompt("How  many hours did you sleep on Sunday?"));
+var sleepEquation=hoursSleptMon+hoursSleptTues+hoursSleptWed+hoursSleptThurs+hoursSleptFri+hoursSleptSat+hoursSleptSun;
+//Your hours slept should equal the hours needed to sleep
+if (sleepEquation<hoursToSleep) {
 //console.log You didn't sleep enough
     console.log("You haven't slept long enough.");
 } else {
-// if your hours slept is greater than or equal to 0
-    if (sleepEquation>=0) {
+// if your hours slept is greater than or equal to the hours needed
+    if (sleepEquation>=hoursToSleep) {
 //console log success
-        console.log("You slept a healthy amount, you should be ready for the day!");
+        console.log("Congratulations, you slept the requried amonut this week!");
     }
 }
