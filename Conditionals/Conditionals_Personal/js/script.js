e682e7129c151a50d9a9df1ea54c4781adb04e9a
Dcampos94 <dlcdcollector@gmail.com>
2014-01-28 19:33:56 -0500

    Setting up variables
    Variables created for Sleep hours

--- Conditionals/Conditionals_Personal/js/script.js
+++ Conditionals/Conditionals_Personal/js/script.js
@@ -2,4 +2,7 @@
  * Created by Daniel on 1/10/14.
  */
 
-document.querySelector("body").stylebackgroundColor= "00FF00";
+//Setting up variables
+var hoursToSleep=parseInt(prompt("How many hours of sleep do you need?"));
+var hoursSlept=parseInt(prompt("How  many hours did you sleep today?"));
+var sleepEquation=hoursToSleep-hoursSlept;
